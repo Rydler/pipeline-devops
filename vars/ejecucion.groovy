6c61edccd58bda8ca3a5c92c677d1ae9d00ca476
Daniel Morales <moralesaguilerad@gmail.com>
2020-12-22 20:46:51 -0300

    Update Jenkinsfile

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -18,7 +18,7 @@ def call(){
                         echo "Stage: ${params.stage}"
                         if (params.stage == '') {
                             println 'INICIO'
-                            println "${params.eleccion}."
+                            "${params.eleccion}.call()"
                             println 'FIN'
                         }
 
