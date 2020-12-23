cffec3231cdcb061364b20634329519d4d065693
Daniel Morales <moralesaguilerad@gmail.com>
2020-12-23 14:15:30 -0300

    Update Jenkinsfile

--- vars/util.groovy
+++ vars/util.groovy
@@ -2,7 +2,7 @@
 def validarStages(validStage,arrayStage){
     
     def check = false
-    //def aux = 0
+    def aux = 0
 
     for (String validValues: validStage){
         for (String values: arrayStage){
