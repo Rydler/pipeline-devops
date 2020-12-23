948d349bc32ec73bfc0a35f38b98945d8fbcdf32
Daniel Morales <moralesaguilerad@gmail.com>
2020-12-22 23:13:14 -0300

    Update Jenkinsfile

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -7,7 +7,7 @@ def call(){
     def arrayStage =  params.stage.split(';')
 
     for(String values : arrayStage){
-        println 'IMPRIMIENDO'
+
 
         switch(values){
             case 'Build':
@@ -16,18 +16,34 @@ def call(){
             }
             break
 
-            case 'esto':
-            println 'ESTO'
+            case 'Sonar':
+            stage('Sonar'){
+                def scannerHome = tool 'sonar';
+                withSonarQubeEnv('sonar') { // If you have configured more than one global server connection, you can specify its name
+                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+                }
+            }
             break
 
-            case 'es':
-            println 'ES'
+            case 'Run':
+            stage('Run'){
+                sh 'nohup bash gradlew  bootRun &'
+                sleep 10
+            }
+            break
+
+            case 'Test':
+            stage('Test'){
+                sh 'curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing'
+            }
+            break
+
+            case 'Nexus':
+            stage('Nexus'){
+                nexusPublisher nexusInstanceId: 'Nexus_server_local', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/var/lib/jenkins/workspace/example_choice/build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.2.gradle']]]
+            }
             break
         }
-    }
-
-    stage('Prueba'){
-        echo 'TESTEANDO...'
     }
 
     /*
