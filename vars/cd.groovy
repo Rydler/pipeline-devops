db505979b4c872f43e07a31950cc38b7a30d8879
Daniel Morales <moralesaguilerad@gmail.com>
2021-01-06 13:02:08 -0300

    Update CI/CD

--- vars/cd.groovy
+++ vars/cd.groovy
@@ -6,17 +6,18 @@ def call(){
 
     stage('runDownloadedJar') {
         sh "nohup java -jar DevOpsUsach2020-1.0.2.gradle.jar & >/dev/null"
-        sh "sleep 20"
+        sh "sleep 10"
     }
-    /*
+    
     stage('rest') {
-        sh './mvnw clean package -e'
+        sh "curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
     }
 
     stage('nexusCD') {
-        nexusPublisher nexusInstanceId: 'Nexus_server_local', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/var/lib/jenkins/workspace/ejemplo-gradle-library/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.2.maven']]]
+        nexusPublisher nexusInstanceId: 'Nexus_server_local', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/var/lib/jenkins/workspace/ci-cd_pipeline-ci_develop/build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.2.release']]]
+
     } 
-    */                    
+                        
 }
 
 return this;
